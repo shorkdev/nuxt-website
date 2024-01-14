1113b57c2bef5f191400982eb5b5f2447fb45370
shork <shork_d3v@proton.me>
2024-01-14 18:30:49 +0300

    trying to fix issues with building the website

--- app.vue
+++ app.vue
@@ -21,34 +21,34 @@ useHead({
   <div
     class="bg-zinc-300 text-black dark:bg-zinc-950 dark:text-white transition-colors h-fit w-screen p-2"
   >
-    <NuxtLayout>
-      <nav class="mb-1 pb-1 text-lg flex items-center">
-        <!-- mb-1 mx-2 pb-1 -->
-        <NuxtLink to="/" class="link">home</NuxtLink>
-        &#160;|&#160;
-        <NuxtLink to="/blog" class="link">blog</NuxtLink>
-        &#160;|
-        <ClientOnly>
-          <UButton
-            :icon="isDark ? 'i-tabler-moon' : 'i-tabler-sun'"
-            color="gray"
-            variant="ghost"
-            size="xs"
-            aria-label="Theme"
-            @click="isDark = !isDark"
-          />
+    <!-- <NuxtLayout> -->
+    <nav class="mb-1 pb-1 text-lg flex items-center">
+      <!-- mb-1 mx-2 pb-1 -->
+      <NuxtLink to="/" class="link">home</NuxtLink>
+      &#160;|&#160;
+      <NuxtLink to="/blog" class="link">blog</NuxtLink>
+      &#160;|
+      <ClientOnly>
+        <UButton
+          :icon="isDark ? 'i-tabler-moon' : 'i-tabler-sun'"
+          color="gray"
+          variant="ghost"
+          size="xs"
+          aria-label="Theme"
+          @click="isDark = !isDark"
+        />
 
-          <template #fallback>
-            <div class="w-8 h-8" />
-          </template>
-        </ClientOnly>
-      </nav>
-      <NuxtPage />
-      <UNotifications />
-      <!-- <div
+        <template #fallback>
+          <div class="w-8 h-8" />
+        </template>
+      </ClientOnly>
+    </nav>
+    <NuxtPage />
+    <UNotifications />
+    <!-- <div
         class="mx-2 h-[95vh] p-4 rounded-xl border-2 border-zinc-400 dark:border-zinc-800 bg-zinc-200 dark:bg-zinc-900"
       >
       </div> -->
-    </NuxtLayout>
+    <!-- </NuxtLayout> -->
   </div>
 </template>
