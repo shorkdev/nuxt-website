5767124e2d48a396280d8f96dacf473c490caf12
shorko <111064437+shorkdev@users.noreply.github.com>
2024-04-08 16:13:14 +0000

    Delete pages/post directory

--- pages/post/01.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>the post page is not done yet. this is just a placeholder page.</h1>
-  </div>
-</template>
--- pages/post/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <main>
-    <h1>post page isn't dont yet.</h1>
-  </main>
-</template>
