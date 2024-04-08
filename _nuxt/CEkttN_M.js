82966c5d1c7954638c3c49082fb1477dd2b64ab8
shorkdev <111064437+shorkdev@users.noreply.github.com>
2024-04-08 16:14:40 +0000

    Deploying to gh-pages from @ shorkdev/nuxt-website@9d1e83676b977d96f337a74d94527367561ce788 🚀

---
+++ _nuxt/CEkttN_M.js
@@ -0,0 +1,1 @@
+import{s as t,G as l,aM as m,aN as r,az as e,o as p,R as u,X as c,aO as d,ag as i,a2 as f}from"./eEb52cPw.js";const y=t({__name:"Modals.client",setup(_){const a=l(m),{isOpen:o}=r();return(V,n)=>e(a)?(p(),u(i(e(a).component),c({key:0},e(a).props,{modelValue:e(o),"onUpdate:modelValue":n[0]||(n[0]=s=>d(o)?o.value=s:null)}),null,16,["modelValue"])):f("",!0)}});export{y as default};
