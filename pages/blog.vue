9d1e83676b977d96f337a74d94527367561ce788
shorko <111064437+shorkdev@users.noreply.github.com>
2024-04-08 16:13:24 +0000

    Delete pages/blog.vue

--- pages/blog.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <title>/blog</title>
-  <div>
-    <h1>blog</h1>
-    <UDivider label="P O S T S" />
-    <NuxtLink to="/post" class="link">01 | information about the blog</NuxtLink>
-  </div>
-</template>
